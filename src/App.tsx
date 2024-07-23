7fef53b8fce6bb6359308483d90aff90e387d2b0
StepPointer <wodoley.94.27@gmail.com>
2024-07-23 10:06:24 +0300

    Update of Initial project Added: typescript, scss, material ui, jest(testing), i18n (languages)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+import { CssBaseline } from "@mui/material";
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <CssBaseline />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import { Suspense, StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './i18n';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Suspense fallback="...is loading">
+            <StrictMode>
+                <App />
+            </StrictMode>
+    </Suspense>
 );
 
 // If you want to start measuring performance in your app, pass a function

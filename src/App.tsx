0a635ebab27bf28fa3c16432c0bbf93621c135c5
StepPointer <wodoley.94.27@gmail.com>
2024-07-26 17:51:46 +0300

    First commit in branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { CssBaseline, Container } from "@mui/material";
+import { ThemeProvider } from '@mui/material/styles';
+
 import './App.scss';
+import './assets/fonts/fonts.scss'
 import Home from "./pages/Home/HomePage";
+import theme from './theme';
 
 function App() {
   return (
     <div className="App">
-        <CssBaseline />
-        <Container  maxWidth='xl'>
-            <Home />
-        </Container>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Container  maxWidth='xl'>
+                <Home />
+            </Container>
+        </ThemeProvider>
     </div>
   );
 }

4cf29da55d274222ab74e79e29177a1918954c59
EGNAROK <egnarok15@gmail.com>
2024-08-10 14:37:41 +0300

    Saving changes before merge

--- src/components/IconCardComponent/IconCardComponent.tsx
+++ src/components/IconCardComponent/IconCardComponent.tsx
@@ -1,38 +1,19 @@
 import React from "react";
 import Card from "./IconCard/IconCard";
-import './IconCard/iconCardStyle.scss'
 import { Container } from "@mui/material";
-import { useTranslation } from "react-i18next";
+import { card1, card2, card3 } from "./IconCardData";
 
-import { 
-  logo_Cheap_it_with_title, 
-  logo_Meal_count_with_title, 
-  logo_Meal_market_logo_with_title, 
-  logo_Gift_box_logo_with_title 
-} from '../../assets/images/';
-
-
-export const IconCardComponent: React.FC = () => {
-  const { t } = useTranslation(["pages/home-page"]);
-  
-  const cards = [
-    { text: t('iconCardComponent.cardText1'), image: logo_Cheap_it_with_title, imageFirst: true },
-    { text: t('iconCardComponent.cardText2'), image: logo_Meal_count_with_title },
-    { text: t('iconCardComponent.cardText3'), image: logo_Meal_market_logo_with_title, imageFirst: true },
-    { text: t('iconCardComponent.cardText4'), image: logo_Gift_box_logo_with_title },  ];
-
+const IconCardComponent: React.FC = () => {
   return (
     <Container maxWidth="xl">
       <div className='icon-card__list'>
-        {cards.map((card, index) => (
-          <Card 
-            key={index} 
-            text={card.text} 
-            image={card.image} 
-            imageFirst={card.imageFirst} 
-          />
-        ))}
+        <Card {...card1}/>
+        <Card {...card2}/>
+        <Card {...card3}/>
       </div>
     </Container>
   );
 };
+
+export default IconCardComponent;
+
---
+++ src/components/IconCardComponent/IconCardData.ts
@@ -0,0 +1,33 @@
+import cheapITIcon from '../../assets/images/CheapItLogoName.png'
+import ecoMealMarketLogo from '../../assets/images/ecoMealMarketLogo.png'
+import mealCountIcon from '../../assets/images/MealCountLogoName.png';
+
+export const card1 = {
+  icon: cheapITIcon,
+  textArray: [
+    'iconCardComponent.set1.cardText1',
+    'iconCardComponent.set1.cardText2',
+    'iconCardComponent.set1.cardText3'
+  ],
+  styleVariant: 1
+};
+
+export const card2 = {
+  icon: ecoMealMarketLogo,
+  textArray: [
+    'iconCardComponent.set2.cardText1',
+    'iconCardComponent.set2.cardText2',
+    'iconCardComponent.set2.cardText3'
+  ],
+  styleVariant: 2
+};
+
+export const card3 = {
+  icon: mealCountIcon,
+  textArray: [
+    'iconCardComponent.set3.cardText1',
+    'iconCardComponent.set3.cardText2',
+    'iconCardComponent.set3.cardText3'
+  ],
+  styleVariant: 3
+};

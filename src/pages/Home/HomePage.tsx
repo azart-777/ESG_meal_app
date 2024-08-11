3073af06e335636626df3648684ad0718254601c
EGNAROK <egnarok15@gmail.com>
2024-08-11 16:27:03 +0300

    done

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -2,7 +2,6 @@ import { ContactUsForm } from '../../components';
 import TreeInfoComponent from '../../components/TreeInfoComponent/TreeInfoComponent';
 import { Header } from "../../components/Header/Header";
 import { PromotionalBanner } from '../../components';
-// import { EarthImage } from '../../components'; {/*Переписав компоненту, в PromotionalBanner*/}
 import { IconCardComponent } from '../../components';
 import { MoveUpArrow } from '../../components';
 
@@ -11,7 +10,6 @@ const Home = () => {
         <>
             <Header />
             <PromotionalBanner/>
-            {/* <EarthImage /> */} {/*Переписав компоненту, в PromotionalBanner*/}
             <TreeInfoComponent />
             <IconCardComponent />
             <ContactUsForm />

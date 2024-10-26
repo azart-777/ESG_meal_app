9b1b67a83ea439493e17ff7105335441c594f24a
EGNAROK <egnarok15@gmail.com>
2024-10-26 14:18:10 +0300

    Button is indented only when the page is at the bottom

--- src/components/MoveUp/MoveUp.tsx
+++ src/components/MoveUp/MoveUp.tsx
@@ -1,16 +1,27 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./MoveUp.scss";
 
 export const MoveUp: React.FC = () => {
   const scrollBtnRef = useRef<HTMLDivElement>(null);
+  const [isBottom, setIsBottom] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       if (scrollBtnRef.current) {
+        const scrollPosition = window.scrollY + window.innerHeight;
+        const documentHeight = document.documentElement.scrollHeight;
+        const offset = 180;
+
         if (window.scrollY > 700) {
           scrollBtnRef.current.classList.remove("move-up-arrow--hidden");
         } else {
           scrollBtnRef.current.classList.add("move-up-arrow--hidden");
+        }
+
+        if (scrollPosition >= documentHeight - offset) {
+          setIsBottom(true);
+        } else {
+          setIsBottom(false);
         }
       }
     };
@@ -23,15 +34,16 @@ export const MoveUp: React.FC = () => {
   }, []);
 
   const handleClick = () => {
-    window.scrollTo(0, 0);
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   return (
     <div
       id="move-up-arrow"
-      className="move-up-arrow move-up-arrow--hidden"
+      className={`move-up-arrow ${isBottom ? "move-up-arrow--bottom" : ""} move-up-arrow--hidden`}
       ref={scrollBtnRef}
       onClick={handleClick}
     ></div>
   );
 };
+

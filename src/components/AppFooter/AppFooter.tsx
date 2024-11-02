8d66f0857de10d9a74a4ea7001488861d297e24d
StepPointer <wodoley.94.27@gmail.com>
2024-11-02 17:38:41 +0200

    Social Links Update

--- src/components/AppFooter/AppFooter.tsx
+++ src/components/AppFooter/AppFooter.tsx
@@ -14,21 +14,21 @@ export const AppFooter: React.FC = () => {
           </p>
         </div>
         <div className="footer__social">
-          <a className="footer__link" href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+          <a className="footer__link" href="https://www.facebook.com/profile.php?id=61566919077712" target="_blank" rel="noopener noreferrer">
             <img src={social.facebook} alt="Facebook" className="footer__icon" />
           </a>
-          <a className="footer__link" href="https://www.apple.com/app-store/" target="_blank" rel="noopener noreferrer">
-            <img src={social.apple} alt="App Store" className="footer__icon" />
-          </a>
-          <a className="footer__link" href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+          {/*<a className="footer__link" href="https://www.apple.com/app-store/" target="_blank" rel="noopener noreferrer">*/}
+          {/*  <img src={social.apple} alt="App Store" className="footer__icon" />*/}
+          {/*</a>*/}
+          <a className="footer__link" href="https://www.instagram.com/cheap_it_family/" target="_blank" rel="noopener noreferrer">
             <img src={social.instagram} alt="Instagram" className="footer__icon" />
           </a>
-          <a className="footer__link" href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+          <a className="footer__link" href="https://www.linkedin.com/company/chi-f" target="_blank" rel="noopener noreferrer">
             <img src={social.linkedin} alt="LinkedIn" className="footer__icon" />
           </a>
-          <a className="footer__link" href="https://play.google.com" target="_blank" rel="noopener noreferrer">
-            <img src={social.google} alt="Google Play" className="footer__icon" />
-          </a>
+          {/*<a className="footer__link" href="https://play.google.com" target="_blank" rel="noopener noreferrer">*/}
+          {/*  <img src={social.google} alt="Google Play" className="footer__icon" />*/}
+          {/*</a>*/}
         </div>
       </div>
     </footer>
